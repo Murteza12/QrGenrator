55a8dea9b4f4a6e4f01d7d138138a1f0dd389341
Murteza <murteza@techbayportal.com>
2020-11-24 23:20:01 +0500

    first commit

--- Qrgenrator/AppDelegate.swift
+++ Qrgenrator/AppDelegate.swift
@@ -9,22 +9,34 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    var window: UIWindow?
+    var redirect :RedirectHelper!
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if #available(iOS 13, *) {
+            //It will start from SceneDelegate
+            
+        } else {
+            
+            let frame = UIScreen.main.bounds
+            self.window = UIWindow(frame: frame)
+            redirect = RedirectHelper(window: window)
+            redirect.determineRoutes(storyboard: .Main)
+            
+        }
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
--- Qrgenrator/ViewController.swift
+++ Qrgenrator/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var qrImage:UIImageView!
+    @IBOutlet weak var qrField:UITextField!
+    
+    var activeField:UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("helooooo")
+        if #available(iOS 13.0, *) {
+            overrideUserInterfaceStyle = .light
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let view = UIView()
+        if #available(iOS 13.0, *) {
+            view.overrideUserInterfaceStyle = .light
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    @IBAction  func QrAction(_ sender: UIButton){
+      
+        self.qrImage.image = self.generateQRCode(from: self.qrField.text ?? "")
+      
+    }
+
+    
+    func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 3, y: 3)
+
+            if let output = filter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: output)
+            }
+        }
+
+        return nil
     }
 
 
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        activeField = textField
+        return true
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        activeField?.resignFirstResponder()
+        activeField = nil
+        return true
+    }
+}
